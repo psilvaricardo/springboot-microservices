41a0c42bfd52cc48a39c5cb96a1633e4f56ed64b
Ricardo Plascencia Silva <ricardo072@gmail.com>
2025-12-28 21:44:24 -0600

    adding new 2026 folder and adding dummy project

---
+++ 2026/dummy-spring-framework-app1/src/main/java/com/example/spring/dummy_spring_framework_app1/DummySpringFrameworkApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring.dummy_spring_framework_app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummySpringFrameworkApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummySpringFrameworkApp1Application.class, args);
+	}
+
+}
---
+++ 2026/dummy-spring-framework-app1/src/test/java/com/example/spring/dummy_spring_framework_app1/DummySpringFrameworkApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.dummy_spring_framework_app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummySpringFrameworkApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
